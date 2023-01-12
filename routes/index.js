45a137eba76111169ef448a05d80b6791ef60e7f
Jessica Ejelöv <jessicaejelov@gmail.com>
2023-01-12 17:56:35 +0100

    db connection maed, get for entries made

--- routes/index.js
+++ routes/index.js
@@ -2,30 +2,37 @@ var express = require('express');
 var router = express.Router();
 
 // DB connection - MongoDB Atlas via mongoose
-const uri = "mongodb+srv://jessofnorth:MorsLillaOlle92@moneytracker.tiojbdq.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://jessofnorth:MoneyTracker@moneytracker.tiojbdq.mongodb.net/?retryWrites=true&w=majority";
 const mongoose = require("mongoose");
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  dbName: 'MoneyTracker',
 });
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise; 
 
 
 let db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function (callback) {
-  console.log("Connected to database");
+  console.log("Connected to server");
   // create DB schema for entrys
-  let entrySchema = mongoose.Schema({
-    name: String,
+  let entrySchema = new mongoose.Schema({
+    title: String,
+    date: String,
+    amount: Number,
   });
   let Entry = mongoose.model("Entry", entrySchema);
 
-
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  // get courses from DB
+  Entry.find(function (err, entrys) {
+    if (err) return console.error(err);
+    let jsonObj = JSON.stringify(entrys);
+    res.contentType("application/json");
+    res.send(jsonObj);
+  });
 });
 
 });
